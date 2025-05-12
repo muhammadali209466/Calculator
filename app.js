f6418e485efce247af699255bc4506e4954f2aa3
muhammadali209466 <muhammadaligamer209@gmail.com>
2025-05-12 17:58:13 +0500

    UI Change

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ function equalTo() {
   input.value = result;
 }
 
-function allClear() {
-  var input = document.getElementById("input");
-  input.value = "";
-}
-
 function delValue() {
   var input = document.getElementById("input");
   var editValue = input.value.slice(0, -1);
   input.value = editValue;
+}
+
+function clearAll() {
+  var input = document.getElementById("input");
+  input.value = "";
 }
